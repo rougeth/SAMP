90e254b8cf0c34e6930cf4e255b5035dca4de926
Marco Rougeth <marco@rougeth.com>
2014-08-31 19:02:57 -0300

    Test bus-stops url to view

--- src/buses/tests.py
+++ src/buses/tests.py
@@ -1,3 +1,13 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
+from buses.views import bus_stops
+
+
 # Create your tests here.
+class BusStopsTest(TestCase):
+
+    def test_url_to_view(self):
+        found = resolve('/bus-stops')
+        self.assertEqual(found.func, bus_stops)
+
