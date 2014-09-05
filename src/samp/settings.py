bc8fdbbd0cef88fab7a03d71e89baf7e113db12b
Marco Rougeth <marco@rougeth.com>
2014-09-05 16:26:18 -0300

    Add geoposition app to handle with lat/long fields

--- src/samp/settings.py
+++ src/samp/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Thrid party
+    'geoposition',
 )
 
 MIDDLEWARE_CLASSES = (
