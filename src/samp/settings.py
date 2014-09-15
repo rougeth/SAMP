942a9bd5afc9530a8d1863daee1de6e924e82738
Marco Rougeth <marco@rougeth.com>
2014-09-14 22:38:48 -0300

    Fix wrong var name

--- src/samp/settings.py
+++ src/samp/settings.py
@@ -84,10 +84,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
 
-TEMPLATES_DIR = (
-    os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
 )
