4b09a8d5c1d36b89b1a48c281e42b9d8b1896609
Marco Rougeth <marco@rougeth.com>
2014-09-14 21:26:26 -0300

    Add static and template folders to settings

--- src/samp/settings.py
+++ src/samp/settings.py
@@ -81,3 +81,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
+
+TEMPLATES_DIR = (
+    os.path.join(BASE_DIR, 'templates')
+)
