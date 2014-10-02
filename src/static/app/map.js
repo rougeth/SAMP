6c5827955713e7db29171deaf2f51fcf33368a48
Marco Rougeth <marco@rougeth.com>
2014-10-02 11:07:04 -0300

    Organized map configs and added function to add a marker in the map

--- src/static/app/map.js
+++ src/static/app/map.js
@@ -1,13 +1,25 @@
 var map;
+var markers = [];
+
 function init_samp_map() {
-  var mapOptions = {
-    maxZoom: 16,
-    minZoom: 14,
-    zoom: 14,
-    streetViewControl: false,
-    center: new google.maps.LatLng(-15.7929449, -47.8882138)
-  };
-  map = new google.maps.Map(document.getElementById('samp-map'), mapOptions);
+    var brasilia = new google.maps.LatLng(-15.7929449, -47.8882138);
+    var mapOptions = {
+        maxZoom: 16,
+        minZoom: 14,
+        zoom: 14,
+        streetViewControl: false,
+        center: brasilia
+    };
+    map = new google.maps.Map(document.getElementById('samp-map'),
+        mapOptions);
+}
+
+function add_marker(locale) {
+    var marker = new google.maps.Marker({
+        position: locale,
+        map: map
+    });
+    markers.push(marker);
 }
 
 google.maps.event.addDomListener(window, 'load', init_samp_map);
