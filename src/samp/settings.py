8fcde668b45089a20335500c40a47614287b5a6b
Marco Rougeth <marco@rougeth.com>
2014-10-03 11:23:39 -0300

    Added rest_framework auth permissions

--- src/samp/settings.py
+++ src/samp/settings.py
@@ -96,3 +96,11 @@ STATICFILES_DIRS = (
 TEMPLATES_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
