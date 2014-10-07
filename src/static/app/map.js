1428f084ce430e763f4ebf6c27f56a3b457ce1e6
Mario <sergio.mario_q@hotmail.com>
2014-10-07 11:55:08 -0300

    Inserted bounds on map.

--- src/static/app/map.js
+++ src/static/app/map.js
@@ -10,15 +10,43 @@ function init_samp_map() {
         streetViewControl: false,
         center: brasilia,
         styles: [
-            {
-                featureType: "transit.station.bus",
-                stylers: [
-                    { visibility: "off" }
-                ]
-            }]
+        {
+            featureType: "transit.station.bus",
+            stylers: [
+            { visibility: "off" }
+            ]
+        }]
     };
+
     map = new google.maps.Map(document.getElementById('samp-map'),
         mapOptions);
+
+   // Bounds for DF
+   var strictBounds = new google.maps.LatLngBounds(
+     new google.maps.LatLng(-15.792, -47.888), 
+     new google.maps.LatLng(-15.7929449, -47.8882138)
+   );
+
+   // Listen for the dragend event
+   google.maps.event.addListener(map, 'dragend', function() {
+    if (strictBounds.contains(map.getCenter())) return;
+
+    var c = map.getCenter(),
+        x = c.lng(),
+        y = c.lat();
+        limit = 0.3
+        maxX = strictBounds.getNorthEast().lng(),
+        maxY = strictBounds.getNorthEast().lat(),
+        minX = strictBounds.getSouthWest().lng(),
+        minY = strictBounds.getSouthWest().lat();
+
+    if (x < minX-limit) x = minX;
+    if (x > maxX+limit) x = maxX;
+    if (y < minY-limit) y = minY;
+    if (y > maxY+limit) y = maxY;
+
+    map.setCenter(new google.maps.LatLng(y, x));
+   });
 }
 
 function add_bus_stop(locale) {
