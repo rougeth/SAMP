380d9da9b38d025222d151d7beba96940f94cfca
Marco Rougeth <marco@rougeth.com>
2014-10-16 11:55:12 -0300

    Update buses url

--- src/buses/urls.py
+++ src/buses/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('buses.views',
-    url(r'^regions/(?P<region_a>[^/]+)/(?P<region_b>[^/]+)/$', 'bus_lines',
-        name='bus_lines'),
+    url(r'^lines$', 'bus_lines', name='bus_lines'),
+    url(r'^lines/(?P<region_a>[^/]+)/(?P<region_b>[^/]+)/$',
+        'bus_lines_per_regions', name='bus_lines_per_regions'),
     url(r'^route/(?P<line>[^/]+)$', 'bus_route', name='bus_route'),
     url(r'^regions$', 'bus_regions', name='bus_regions'),
     url(r'^stops$', 'bus_stops', name='bus_stops'),
