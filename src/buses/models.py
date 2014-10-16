1e01d9b5b03fe70b69c22f99db7ea2ded951a63d
Marco Rougeth <marco@rougeth.com>
2014-10-16 11:54:05 -0300

    Update buses Stop model

--- src/buses/models.py
+++ src/buses/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
 
-class Stop(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-
-
 class Region(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+
+class Stop(models.Model):
+    region = models.ForeignKey(Region)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
 
 
 class Line(models.Model):
